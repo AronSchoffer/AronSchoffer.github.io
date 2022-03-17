e9c77795fdf8b918552f6272c64b933b0d0dcf81
Aron Schöffer <aronschoffer@gmail.com>
2022-03-17 12:58:31 +0100

    added framboos button

--- src/logger/Buttons.svelte
+++ src/logger/Buttons.svelte
@@ -2,6 +2,7 @@
   {#each buttons as but}
     <button on:click={logPoint} name={but}> {but} </button> 
   {/each}
+  <button on:click={logPoint} use:longpress={logPoint} on:longpress={resetF} name='framboos' > framboos {f} </button>
   <Track/>
   <button on:click={reset}> reset logId </button>
 </div>
@@ -13,10 +14,14 @@
   
   let gjson;
   let buttons = ['oost', 'O-hoek', 'N-hoek', 'noord', 'ingang', 'other button'];
+  let f = 1;
+  let fHasClick = true;
   
   function logPoint(e) {
     if (e.target.name === 'other button') {
       var name = prompt('Naam meetpunt:');
+    } else if (e.target.name === 'framboos'){
+      var name = 'framboos ' + f;
     } else {
       var name = e.target.name.trim();
     }
@@ -62,6 +67,7 @@
     var id = gjson.properties.id;
     var exportName = id + '_' + gjson.properties.name;
     if (confirm('Would you like to save ' + exportName + '?')) {
+      if (gjson.properties.name.includes('framboos')) f = f % 5 + 1;
       downloadObjectAsJson(gjson, exportName);
       //prepare for next log
       if (gjson.properties.name != '' || null) {
@@ -100,6 +106,48 @@
     var logId = prompt('Reset current logId of ' + localStorage.getItem('logId') + ' to:');
     if(logId) localStorage.setItem('logId', logId);
   }
+  
+  //https://stackoverflow.com/questions/56844807/svelte-long-press
+  function longpress(node, clickFn, threshold = 1000) {
+  // note — a complete answer would also consider touch events
+
+    const handle_mousedown = () => {
+      let start = Date.now();
+
+      const timeout = setTimeout(() => {
+        node.removeEventListener('click', clickFn);
+        fHasClick = false;
+        console.log('click removed');
+        node.dispatchEvent(new CustomEvent('longpress'));
+        // console.log(getEventListeners(node));
+      }, threshold);
+
+      const cancel = () => {
+        clearTimeout(timeout);
+        if (!fHasClick) {
+          setTimeout(function () {
+            console.log('add click');
+            node.addEventListener('click', clickFn);  
+          }, 200);
+        }
+        node.removeEventListener('mouseup', cancel);
+      };
+
+      node.addEventListener('mouseup', cancel);
+    }
+
+    node.addEventListener('mousedown', handle_mousedown);
+
+    return {
+      destroy() {
+        node.removeEventListener('mousedown', handle_mousedown);
+      }
+    };
+  }
+
+function resetF() {
+  f = 1;
+}
 </script>
 
 <style>
